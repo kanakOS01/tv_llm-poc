5d6ef03fd7041ed70fd5671c5bc55c4bf46abe79
Kanak Tanwar <kanaktanwarpro@gmail.com>
2025-03-22 20:52:50 +0530

    add code execution logic

--- llm/api.py
+++ llm/api.py
@@ -40,7 +40,28 @@ class LLM:
                     "system",
                     """You are a helpful assistant with knowledge about Tolvera. Tölvera is a Python library designed for composing together and interacting with basal agencies, inspired by fields such as artificial life (ALife) and self-organising systems. It provides creative coding-style APIs that allow users to combine and compose various built-in behaviours, such as flocking, slime mold growth, and swarming, and also author their own.
 
-Your task is to generate tolvera code according to the user's query.""",
+Your task is to generate tolvera code according to the user's query.
+
+Example Tolvera code - 
+import taichi as ti
+from tolvera import Tolvera, run
+
+def main(**kwargs):
+    tv = Tolvera(**kwargs)
+
+    @ti.kernel
+    def draw():
+        w = 100
+        tv.px.rect(tv.x/2-w/2, tv.y/2-w/2, w, w, ti.Vector([1., 0., 0., 1.]))
+
+    @tv.render
+    def _():
+        tv.p()
+        draw()
+        return tv.px
+
+if __name__ == '__main__':
+    run(main)""",
                 ),
                 MessagesPlaceholder(variable_name="messages"),
             ]
--- llm/app.py
+++ llm/app.py
@@ -1,12 +1,20 @@
 import streamlit as st
+
 from api import LLM
+from executor import CodeExecutor
+
+
 llm = LLM()
+code_executor = CodeExecutor()
+
 
 st.set_page_config(page_title="Tolvera LLM Chat")
 st.title("💬 Chat with Tolvera-Integrated LLM")
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = []
+if "code_block" not in st.session_state:
+        st.session_state["code_block"] = None
 
 for message in st.session_state["messages"]:
     with st.chat_message(message["role"]):
@@ -21,13 +29,19 @@ if user_input:
     response_text = response_container.empty()  
     streamed_response = ""
 
-    for chunk in llm.generate_response(user_input):
-        streamed_response += chunk
-        response_text.markdown(streamed_response)
+    with st.spinner("Generating response..."):
+        for chunk in llm.generate_response(user_input):
+            streamed_response += chunk
+            response_text.markdown(streamed_response)
 
     st.session_state["messages"].append({"role": "assistant", "content": streamed_response})
 
     if "```python" in streamed_response:
-        code_block = streamed_response.split("```python")[1].split("```")[0]
-        st.code(code_block, language="python")
+        st.session_state["code_block"] = streamed_response.split("```python")[1].split("```")[0]
 
+if st.session_state["code_block"]:
+
+    if st.button("Run Code"):
+        with st.spinner("Executing code..."):
+            output = code_executor.save_and_execute(st.session_state["code_block"])
+            st.text_area("Execution Output", output, height=200)
